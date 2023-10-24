d4bc3a5904a8e621e00859abbdf3e55e1c2edbe7
kin018 <60412598+kin018@users.noreply.github.com>
2023-10-24 18:20:33 -0400

    Created and Modified React Components

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//PascalCasing when creating a react function component
+function Message() {
+  //JSX: JavaScript XML
+  let name = 'Me'
+  
+  return <h1>Hello {name}</h1>;
+}
+
+export default Message;
