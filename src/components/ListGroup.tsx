bf035c25a355e704630bcead319e8f02d7236005
kin018 <60412598+kin018@users.noreply.github.com>
2023-10-24 21:00:04 -0400

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,25 @@
 import { Fragment } from 'react';
 
 function ListGroup() {
-  const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+  let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
 
+  fruits = [];
+
+  //const message = fruits.length === 0 ? <p>No fruit found</p> : null
+  //or this below either do the same thing only thing is it would be {message} instead
+  const getMessage = () => {
+    fruits.length === 0 && <p>No fruit found</p>;
+  };
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {fruits.map((fruit) => (
           <li key={fruit}>{fruit}</li>
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
